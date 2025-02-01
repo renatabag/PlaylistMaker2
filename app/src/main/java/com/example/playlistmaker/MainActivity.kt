4efe894e2aa445cbfb47f25d8d37fd5a72e2c846
renatabagdanova <bagdanova.renata@gmail.com>
2025-02-01 14:51:49 +0300

    ДЗ2

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button1 = findViewById<MaterialButton>(R.id.button1)
+        val button2 = findViewById<MaterialButton>(R.id.button2)
+        val button3 = findViewById<MaterialButton>(R.id.button3)
+
+        button1?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажата кнопка ПОИСК", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        button2?.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажата кнопка МЕДИАТЕКА", Toast.LENGTH_SHORT).show()
+        }
+
+        button3?.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажата кнопка НАСТРОЙКИ", Toast.LENGTH_SHORT).show()
         }
     }
 }
