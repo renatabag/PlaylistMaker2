214268b6507742ae287d9925dc9942f36e054cc6
renatabagdanova <bagdanova.renata@gmail.com>
2025-02-05 15:59:41 +0300

    shareApp_button и support_button

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,14 +1,13 @@
 package com.example.playlistmaker
-
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 
 class SettingsActivity : AppCompatActivity() {
-    @SuppressLint("WrongViewCast")
+    @SuppressLint("WrongViewCast", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings)
@@ -20,6 +19,37 @@ class SettingsActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        val shareButton = findViewById<MaterialButton>(R.id.button_share_settings)
+        shareButton.setOnClickListener {
+            val shareIntent = shareApp()
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.share_app)))
+        }
+
+        val supportButton = findViewById<MaterialButton>(R.id.support_button)
+        supportButton?.setOnClickListener {
+            val developerEmail = getString(R.string.developer_email)
+            val subject = getString(R.string.support_email_subject)
+            val body = getString(R.string.support_email_body)
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(developerEmail))
+                putExtra(Intent.EXTRA_SUBJECT, subject)
+                putExtra(Intent.EXTRA_TEXT, body)
+            }
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
+        }
+
     }
 
+    private fun shareApp(): Intent {
+        val shareText = getString(R.string.adress_practicum)
+        return Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, shareText)
+            type = "text/plain"
+        }
+    }
 }
