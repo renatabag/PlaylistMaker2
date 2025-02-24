a2380e7ed007788a8196e4bfb4eff8dc39fc5b7a
renatabagdanova <bagdanova.renata@gmail.com>
2025-02-24 21:30:18 +0300

    Commit with using Glide

---
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -0,0 +1,6 @@
+package com.example.playlistmaker
+
+data class Track(val trackName: String,
+                 val artistName: String,
+                 val trackTime: String,
+                 val artworkUrl100: String)
