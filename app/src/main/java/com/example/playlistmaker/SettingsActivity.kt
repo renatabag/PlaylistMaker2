0e48c5f1811bade2020415087eee064b76d97af2
renatabagdanova <bagdanova.renata@gmail.com>
2025-02-28 02:47:23 +0300

    New Commit (1)

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,19 +1,45 @@
 package com.example.playlistmaker
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
     @SuppressLint("WrongViewCast", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings)
+
+        val switchMaterial: SwitchMaterial = findViewById(R.id.mySwitch)
+
+        val states = arrayOf(
+            intArrayOf(android.R.attr.state_checked),
+            intArrayOf(-android.R.attr.state_checked)
+        )
+
+        val colors = intArrayOf(
+            ContextCompat.getColor(this, R.color.blue_switch),
+            ContextCompat.getColor(this, R.color.grey_switch),
+        )
+        val colorsTrack = intArrayOf(
+            ContextCompat.getColor(this, R.color.blue_switch_light),
+            ContextCompat.getColor(this, R.color.grey_switch_light)
+        )
+
+        val thumbColorStateList = ColorStateList(states, colors)
+        val trackColorStateList = ColorStateList(states,colorsTrack)
+        switchMaterial.thumbTintList = thumbColorStateList
+        switchMaterial.trackTintList = trackColorStateList
+
+
         lateinit var inputEditText: EditText
         var searchText: String = ""
         val textWatcher = object : TextWatcher {
